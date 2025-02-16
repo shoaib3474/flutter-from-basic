93fc02486fcf74eb29ce88afeac25aa99108c827
shoaib3474 <shoaib3974@gmail.com>
2025-02-17 02:18:55 +0500

    getting data from rest apis using get method

--- lib/api/get_api.dart
+++ lib/api/get_api.dart
@@ -1,22 +1,62 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-callApi() {
-  final String url = '';
+Future<List<dynamic>> callApi() async {
+  final String url = 'https://jsonplaceholder.typicode.com/posts';
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to load data');
+  }
 }
 
-class GetApi extends StatelessWidget {
+class GetApi extends StatefulWidget {
   const GetApi({super.key});
+
+  @override
+  _GetApiState createState() => _GetApiState();
+}
+
+class _GetApiState extends State<GetApi> {
+  late Future<List<dynamic>> futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    futureData = callApi();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Get API Example'),
+      ),
       body: Center(
-        child: Column(
-          children: [
-            Text('data'),
-            ElevatedButton(onPressed: () {}, child: Text('Tap me '))
-          ],
+        child: FutureBuilder<List<dynamic>>(
+          future: futureData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Text('No data found');
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index]['title']),
+                    subtitle: Text(snapshot.data![index]['body']),
+                  );
+                },
+              );
+            }
+          },
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_from_basic/flex_layout/home_page.dart';
+import 'package:flutter_from_basic/api/get_api.dart';
 import 'package:flutter_from_basic/state_full_and_state_less/post_list_page.dart';
-import 'package:flutter_from_basic/state_full_and_state_less/state_full.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //materialApp is app container
     return MaterialApp(
-      home: PostListPage(),
+      home: GetApi(),
       debugShowCheckedModeBanner: false,
     );
   }

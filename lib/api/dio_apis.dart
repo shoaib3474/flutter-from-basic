b7b81fb4cce623532bc940c436d82e273d05e5ed
shoaib3474 <shoaib3974@gmail.com>
2025-02-17 15:44:18 +0500

    delete unnecessary files

---
+++ lib/api/dio_apis.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DioApis extends StatelessWidget {
+  const DioApis({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
